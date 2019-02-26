5c360a169a3a9dfe8e40be8ee7887c2b63033152
Majer9lazer <sidorenkoegor1999@mail.ru>
2019-02-26 23:25:27 +0600

    добавлена новая весрсия сайта

---
+++ NewsParserSite.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewsParserSite.WEB.MvcApplication" Language="C#" %>
---
+++ NewsParserSite.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsParserSite.WEB.Startup))]
+namespace NewsParserSite.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
